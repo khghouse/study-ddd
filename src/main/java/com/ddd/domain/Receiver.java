e76afacc14da79d9c0f1f4e57c3e9ce2753c33b3
khghouse <khghouse@naver.com>
2022-12-28 22:56:29 +0900

    Value Type을 이용하여 ShippingInfo 필드 구현

---
+++ src/main/java/com/ddd/domain/Receiver.java
@@ -0,0 +1,14 @@
+package com.ddd.domain;
+
+import lombok.Data;
+
+@Data
+public class Receiver {
+    private String name;
+    private String phoneNumber;
+
+    public Receiver(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
--- src/main/java/com/ddd/domain/ShippingInfo.java
+++ src/main/java/com/ddd/domain/ShippingInfo.java
@@ -4,9 +4,6 @@ import lombok.Data;
 
 @Data
 public class ShippingInfo {
-    private String receiverName;
-    private String receiverPhoneNumber;
-    private String shippingAddress1;
-    private String shippingAddress2;
-    private String shippingZipcode;
+    private Receiver receiver;
+    private Address address;
 }
